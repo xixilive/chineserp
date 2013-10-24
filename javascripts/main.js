674314f157d336da8a5458d14a7ddbde93b320c5
xixilive <xixilive@gmail.com>
2013-10-24 16:10:43 +0800

    update gh-page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log('This would be the main JS file.');
+$(function(){
+  var picker = new ChineseRegion.RegionPicker({
+    remote: 'json',
+    initialized: function(){
+      $('#pick').on('change', function(){
+        $('#picked').empty();
+        if(!$(this).val()){
+          return;
+        }
+        $('#pick').attr('disabled', true);
+        picker.pick($(this).val(), function(regions){
+          $('#pick').attr('disabled', false);
+          $('#picked').append('<h3>'+ (regions.map(function(r){ return r.n; }).join(' > ')) +'</h3>');
+          regions.forEach(function(r){
+            $('#picked').append('<hr>');
+            $('<ul>').append('<li>ID: '+r.i+'</li>')
+              .append('<li>Name: '+r.n+'</li>')
+              .append('<li>Alias: '+r.a+'</li>')
+              .append('<li>Pinyin: '+r.y+'</li>')
+              .append('<li>Abbr: '+r.b+'</li>')
+              .append('<li>Postcode: '+r.z+'</li>').appendTo($('#picked'));
+          });
+        });
+      });
+    }
+  });
+});
